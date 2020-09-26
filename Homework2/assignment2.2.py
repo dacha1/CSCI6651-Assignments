8590d6542fee02a8a038879a2e78d964d8e993f7
Dinesh <dacha1@unh.newhaven.edu>
2020-09-26 00:01:05 -0400

    assignment 4.1, assignment 4.2, assignment 4.3

--- Homework2/assignment2.2.py
+++ Homework2/assignment2.2.py
@@ -1,15 +1,11 @@
-def takeString(s):
-    index = [4,5,6,7,0,2,1,3]
-    a = list(s)
-    print(a)
-    b = []
-    for i in index:
-        b.append(a[i])
-    x = "".join(b)
-    print(x)
-
-takeString('codeleet')
-
+s = "codeleet"
+indices = [4,5,6,7,0,2,1,3]
+result = [0] * len(s)
+ls = list(s)
+print(ls)
+for i in range(len(s)):
+    result[indices[i]] = ls[i]
+print(''.join(result))    
 
 
 
